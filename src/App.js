b137bb48cc1b666df8fe7d826795d771687cbfc0
Alejandro Sanchez <alejandroszg@gmail.com>
2023-08-10 18:05:51 -0400

    second lesson of children in react

--- src/App.js
+++ src/App.js
@@ -37,25 +37,16 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#FFF"
-              onClick={handlePrevious}
-              text="Previous"
-              emoji="👈🏼"
-            />
-            <Button
-              bgColor="#7950f2"
-              textColor="#FFF"
-              onClick={handleNext}
-              text="Next"
-              emoji="👉🏼"
-            />
+            <Button bgColor="#7950f2" textColor="#FFF" onClick={handlePrevious}>
+              <span>👈🏼</span> Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#FFF" onClick={handleNext}>
+              Next <span>👉🏼</span>
+            </Button>
           </div>
         </div>
       )}
@@ -63,13 +54,22 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, text, emoji }) {
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
       onClick={onClick}
     >
-      <span>{emoji}</span> {text}
+      {children}
     </button>
   );
 }
